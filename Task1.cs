4ec33ba083eeeab2aba3cc0ae4b6a6a28b1e34bc
Andrey Zaraev <zxc-17@yandex.ru>
2024-03-29 16:31:41 +0500

    add task1

--- Task1.cs
+++ Task1.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Task1
+{
+    public static void Main(string[] args)
+    {
+        int m;
+        int n;
+        if (!CheckParams(args, out m, out n)) return;
+        PrintDigit(m, n);
+    }
+
+    private static void PrintDigit(int m, int n)
+    {
+        Console.WriteLine(m);
+        if (m < n)
+            PrintDigit(m + 1, n);
+    }
+
+    private static bool CheckParams(string[] args, out int m, out int n)
+    {
+        n = 0;
+        m = 0;
+        if (int.TryParse(args[0], out m) && int.TryParse(args[1], out n))
+        {
+            if (m < 1 || n < 1)
+            {
+                Console.WriteLine("Неверные данные: Числа должны быть натуральными.");
+                return false;
+            }
+            if (m > n)
+            {
+                Console.WriteLine("Неверные данные: Второй параметр не может быть меньше первого.");
+                return false;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверные данные: Необходимы два натуральных числа.");
+            return false;
+        }
+        return true;
+    }
+}
